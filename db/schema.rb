1476b0c49f115698f5c0c157a5e55e9bea38d610
Nishant <neo.nishant7@gmail.com>
2015-01-26 10:47:22 +0000

    Added username, first name and last name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150126071812) do
+ActiveRecord::Schema.define(version: 20150126102943) do
 
   create_table "comments", force: true do |t|
     t.text     "comment"
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(version: 20150126071812) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "user_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
