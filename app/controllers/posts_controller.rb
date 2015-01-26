2a6cab51249876a76d1ded584b2da3875b2a819d
Nishant <neo.nishant7@gmail.com>
2015-01-26 07:16:28 +0000

    add authentication to application

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   
   
   def index
