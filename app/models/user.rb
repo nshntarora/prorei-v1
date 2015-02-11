b450f7518def05dd1e5046f9e4e755d56108098b
Nishant <neo.nishant7@gmail.com>
2015-02-11 10:46:36 +0000

    Added validation for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  validates :user_name, uniqueness: true
+  
   has_many :posts
   has_many :comments
 end
