88ce566f522f12c47a5fdb4257dff46ef26b85d8
Nishant <neo.nishant7@gmail.com>
2015-02-28 12:35:21 +0000

    Built the temporary homepage and initial bootstrap integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   devise_for :users
   resources :users
- resources :posts do
+  resources :posts do
   resources :comments
   end
- root 'posts#index'
+ root 'static_pages#home'
 end
