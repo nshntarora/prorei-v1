89c1a03ed40dbfae91909ebb9abed823ade1340f
Nishant Arora <neo.nishant7@gmail.com>
2015-03-19 14:07:15 +0530

    Installed paperclip. validation errors to be tackled

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318180324) do
+ActiveRecord::Schema.define(version: 20150319081327) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -34,6 +34,10 @@ ActiveRecord::Schema.define(version: 20150318180324) do
     t.integer  "mobile_number"
     t.string   "phone"
     t.text     "about"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
