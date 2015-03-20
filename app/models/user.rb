58a7e185cfb156fd76203f296544d9bce7a82467
Nishant Arora <neo.nishant7@gmail.com>
2015-03-20 10:59:00 +0530

    No avatars everything else works fine

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,12 +13,4 @@ class User < ActiveRecord::Base
   			numericality: {only_integer: true}
   validates :about,
         length: {maximum: 250, message: "You've got 250 chars, GET CREATIVE"}
-
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
-  # Validate content type
-  validates_attachment_content_type :avatar, :content_type => /\Aimage/
-  # Validate filename
-  validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
-  # Explicitly do not validate
-  do_not_validate_attachment_file_type :avatar
 end
