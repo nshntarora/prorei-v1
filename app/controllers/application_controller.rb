515380292f91e41f53ba95497e948a06a8a2583d
Nishant Arora <neo.nishant7@gmail.com>
2015-05-22 14:21:45 +0530

    User profile styling 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :user_name, :phone, :email, :password) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :user_name, :word1, :word2, :word3, :city, :phone, :current_password, :email, :password, :password_confirmation, :about, :avatar, :avatar_cache, :remove_avatar, :achievement1, :achievement2, :achievement3, :title1, :description1, :link1, :title2, :description2, :link2, :title3, :description3, :link3, :skill1, :skill2, :skill3) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :word1, :word2, :word3, :city, :phone, :current_password, :email, :password, :password_confirmation, :about, :avatar, :avatar_cache, :remove_avatar, :achievement1, :achievement2, :achievement3, :title1, :description1, :link1, :title2, :description2, :link2, :title3, :description3, :link3, :skill1, :skill2, :skill3) }
   end
 end
