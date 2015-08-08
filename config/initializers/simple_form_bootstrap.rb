cd6776a4ffafb68b406db04631e7b6a3a60f5aaf
Nishant Arora <neo.nishant7@gmail.com>
2015-08-08 14:23:30 +0530

    Added Alert CSSp

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,6 +1,6 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
-  config.error_notification_class = 'alert alert-danger'
+  config.error_notification_class = 'alert alert-alert'
   config.button_class = 'btn btn-default'
   config.boolean_label_class = nil
 
