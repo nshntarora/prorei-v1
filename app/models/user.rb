4cbe9c3e4dd31fba45d7eb9edc48c032bb8c4ac1
Nishant Arora <neo.nishant7@gmail.com>
2015-08-08 13:41:52 +0530

    Solved the signup issue - Wrong Validations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,6 @@ class User < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  validates :user_name,
-            uniqueness: true,
-            format: {with: /\A[a-zA-Z0-9]+\Z/ }
   validates :phone,
   			presence: true,
   			length: {is: 10},
@@ -17,7 +13,6 @@ class User < ActiveRecord::Base
   validates :about,
         length: {maximum: 250, message: "You've got 250 chars, GET CREATIVE"}
 
-  validates_presence_of   :avatar
   validates_integrity_of  :avatar
   validates_processing_of :avatar
   has_many :responses
